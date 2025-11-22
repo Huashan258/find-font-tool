4c8089df59f477227a72958f25c25e3f9204533a
Huashan258 <1095832103@qq.com>
2025-11-23 02:59:10 +0800

    Upload file in 2025-11-23

---
+++ 上传主分支.bat
@@ -0,0 +1,11 @@
+@echo
+git add .
+
+set "year=%date:~0,4%"
+set "month=%date:~5,2%"
+set "day=%date:~8,2%"
+set "formatted_date=%year%-%month%-%day%"
+
+git commit -m "Upload file in %formatted_date%"
+git push -u origin main
+pause
